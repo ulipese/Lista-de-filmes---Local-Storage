6c5c8d06e53b0fdba2b20130f9be0f15b335dccb
Caio Oliveira <caio.oliveira07@gmail.com>
2024-03-19 15:01:31 -0300

    Base de filmes adicionada

---
+++ script.js
@@ -0,0 +1,35 @@
+/*
+Criando a base de dados de filmes  
+*/
+
+const filmes = [
+    {
+    id: 0,
+    nome: 'Harry Potter',
+    genero: 'fantasia',
+    lancamento: 2001
+    },
+    {
+    id: 1,
+    nome: 'Avatar',
+    genero: 'fantasia',
+    lancamento: 2010
+    },
+    {
+     id:2,
+     nome:'O senhor dos Anéis',
+     genero: 'fantasia',
+     lancamento: 2000,
+    },
+    {
+     id:3,
+     nome: 'Branquelas',
+     genero: 'comédia',
+     lancamento: 2007
+    },
+    {
+     id:4,
+     nome: 'A Lagoa Azul',
+     genero: 'romance',
+     lancamento: 1983
+}]
