cf101c51320a00ac7832f9ea0dbf1a48be4e70b1
Caio Oliveira <caio.oliveira07@gmail.com>
2024-03-19 15:03:00 -0300

    Adicionando renderização da lista na tela

--- script.js
+++ script.js
@@ -47,3 +47,44 @@ Pegando Elementos HTML
 const btn1 = document.querySelector('button')
 //pega a lista de filmes
 const listaFilmes = document.querySelector('#listaFilmes')
+
+/*
+Ao carregar a página, executa a função que renderiza os elementos na tela
+*/
+
+window.onload = () => {
+   renderizarLista()
+}
+
+/*
+Função para renderizar filmes na tela
+*/
+
+
+const renderizarLista = () =>{
+    //limpa a tela antes de renderizar
+    listaFilmes.innerHTML = ""
+    //percorre o array de filmes, inserindo um li com o nome do filme a cada volta do loop
+    filmes.forEach((filme)=>{
+        const itemLista = document.createElement('li')
+        //adiciona o li à lista de filmes
+        listaFilmes.append(itemLista)
+        //adiciona o filme que o usuário digitou à lista
+        itemLista.innerHTML = `Meu filme ${filme.nome}`
+
+        
+        //cria uma nova imagem
+        const favorito = document.createElement('img')
+        //adiciona imagem ao item img
+        favorito.src = 'img/heart.svg'
+        //muda o cursor da imagem para mãozinha de clique
+        favorito.style.cursor = 'pointer'
+        //adiciona evento de clique à imagem
+        favorito.addEventListener('click',(e)=>{
+            favoritoClicado(e,filme)
+        })
+        //adiciona a imagem ao item da lista
+        itemLista.append(favorito)
+    })
+}
+
